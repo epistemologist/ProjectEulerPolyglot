358e63d0e6f1f10283286111720da7a115c73086
epistemologist <hhrraazzaa@gmail.com>
2020-02-14 01:12:18 -0600

    Init commit

---
+++ bf_utilities.py
@@ -0,0 +1,6 @@
+def swap(x,y,temp):
+	# assuming the pointer starts at temp
+	f = lambda n: ">"*n if n>0 else "<"*abs(n)
+	return "[-]"+f(x-temp)+"[-]"+f(y-x)+"["+f(x-y)+"+"+f(temp-x)+"+"+f(y-temp)+"-]"+f(temp-y)+"["+f(y-temp)+"+"+f(temp-y)+"-]"
+
+print(swap(2,9,3))
