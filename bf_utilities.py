7899fdf3f2c0120246b1d0a95bf7ef2a1b68eb31
Husnain Raza <husnain2@linux-a2.ews.illinois.edu>
2020-03-25 00:39:34 -0500

    Problem 7: bc

--- bf_utilities.py
+++ bf_utilities.py
@@ -3,4 +3,4 @@ def swap(x,y,temp):
 	f = lambda n: ">"*n if n>0 else "<"*abs(n)
 	return "[-]"+f(x-temp)+"[-]"+f(y-x)+"["+f(x-y)+"+"+f(temp-x)+"+"+f(y-temp)+"-]"+f(temp-y)+"["+f(y-temp)+"+"+f(temp-y)+"-]"
 
-print(swap(2,9,3))
+print(swap(2,8,7))
